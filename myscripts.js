029350f1879963b652f96dc819e35c07e947c5b7
vaggelis27 <e.ntosikas@gmail.com>
2025-10-04 00:43:16 +0300

    update 04/10

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
