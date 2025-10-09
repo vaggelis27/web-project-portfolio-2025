61f4779b2a920020e929a69aaaa6020e63c35f95
vaggelis27 <e.ntosikas@gmail.com>
2025-10-10 01:43:46 +0300

    update 10/10

--- myscripts.js
+++ myscripts.js
@@ -1,19 +1,10 @@
 document.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
+  if (!navbar) return;
+
   if (window.scrollY > 50) {
     navbar.classList.add("navbar-scrolled");
   } else {
     navbar.classList.remove("navbar-scrolled");
   }
 });
-
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
